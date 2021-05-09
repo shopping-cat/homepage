50c27530db9733a2fd552156bc9a17f583882200
hyun <coderhyun476@gmail.com>
2021-05-09 23:35:40 +0900

    update homes creen

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,17 @@
 import React from 'react'
+import { APPSTORE_URL, PLAYSTORE_URL } from '../constants/value'
 import styles from '../styles/Home.module.css'
 
 const Home = () => {
-
-    if (typeof window === 'undefined') return null
-
     return (
-        <div>
-            {/* <div>
-                <h1 className={styles.title}>쇼핑냥이</h1>
-                <h1 className={styles.title}>오직 집사만을 위한 쇼핑몰</h1>
-            </div>
-             */}
-            <div>김태훈 안녕 ㅋ</div>
-            <iframe src='https://novelpia.com/' width={window.outerWidth} height={window.outerHeight} />
+        <div className={styles.container} >
+            <img src='icon.png' width={'10%'} />
+
+            <div className={styles.title}>쇼핑냥이</div>
+            <div className={styles.subTitle}>오직 집사만을 위한 쇼핑몰</div>
+
+            <a href={APPSTORE_URL} ><div className={styles.downloadBtn} >iPhone 다운</div></a>
+            <a href={PLAYSTORE_URL} ><div className={styles.downloadBtn}>Android 다운</div></a>
         </div>
     )
 }
---
+++ pages/support.tsx
@@ -0,0 +1,16 @@
+import React, { useEffect } from 'react'
+
+const support = () => {
+
+    useEffect(() => {
+        window.location.href = 'http://pf.kakao.com/_ILHqs/chat'
+    }, [])
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default support
