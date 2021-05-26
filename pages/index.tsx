e1fba49be2dc6bfa4351155dc0c0ce15767168c3
hyun <coderhyun476@gmail.com>
2021-05-26 21:57:56 +0900

    company info

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,26 +12,52 @@ const Home = () => {
             <meta property='description' content='오직 집사만을 위한 쇼핑몰 쇼핑냥이 입니다' />
         </Head>
         <div className={styles.container} >
+            <div className={styles.infoContainer} >
+                <img
+                    src='/iPhoneMockup.png'
+                    alt='mockup'
+                    className={styles.backgroundImage}
 
-            <img
-                src='/iPhoneMockup.png'
-                alt='mockup'
-                className={styles.backgroundImage}
-            />
-            <div className={styles.right} >
-                <h1 className={styles.title}>쇼핑냥이</h1>
-                <h2 className={styles.subTitle}>오직 집사만을 위한 쇼핑몰</h2>
+                />
+                <div className={styles.right} >
+                    <h1 className={styles.title}>쇼핑냥이</h1>
+                    <h2 className={styles.subTitle}>오직 집사만을 위한 쇼핑몰</h2>
+                </div>
+                <div className={styles.left} >
+
+                    <a href={APPSTORE_URL} ><div className={styles.downloadBtn} >
+                        <IoLogoApple size={24} fill={GRAY} />
+                        <div className={styles.downloadText} >App Store</div>
+                    </div></a>
+                    <a href={PLAYSTORE_URL} ><div className={styles.downloadBtn} >
+                        <IoLogoGooglePlaystore size={24} fill={GRAY} />
+                        <div className={styles.downloadText} >Google Play</div>
+                    </div></a>
+                </div>
             </div>
-            <div className={styles.left} >
-
-                <a href={APPSTORE_URL} ><div className={styles.downloadBtn} >
-                    <IoLogoApple size={24} fill={GRAY} />
-                    <div className={styles.downloadText} >App Store</div>
-                </div></a>
-                <a href={PLAYSTORE_URL} ><div className={styles.downloadBtn} >
-                    <IoLogoGooglePlaystore size={24} fill={GRAY} />
-                    <div className={styles.downloadText} >Google Play</div>
-                </div></a>
+            <div className={styles.bottom} >
+                <div>
+                    <h3>문의</h3>
+                    <a target='_blank' href="https://www.notion.so/73fcfb96cc914f6f82f254c119a034a3" >입점 안내</a>
+                    <a target='_blank' href="mailto:coderhyun476@gmail.com">이메일</a>
+                    <a target='_blank' href="http://pf.kakao.com/_ILHqs/chat" >카카오톡</a>
+                    <a target='_blank' href="mailto:coderhyun476@gmail.com">광고 / 제휴</a>
+                    <a target='_blank' href="https://www.notion.so/e50479aa12274e3c9001ff46947aa5bf">채용</a>
+                </div>
+                <div>
+                    <h3>쇼핑냥이</h3>
+                    <span>대표자 남궁현 | 사업자 등록번호 698-47-00632</span>
+                    <span>대표자번호 050-7465-0492</span>
+                    <span>통신판매업</span>
+                    <span  >
+                        <a target='_blank' href="https://shoppingcat.kr/privacy">개인정보 처리방침</a>
+                        <span style={{ color: GRAY }} > | </span>
+                        <a target='_blank' href="https://shoppingcat.kr/agreement">서비스 이용약관</a>
+                        <span style={{ color: GRAY }} > | </span>
+                        <a target='_blank' href="https://shoppingcat.kr/orderCancelInfo">주문/교환/환불 안내</a>
+                    </span>
+                    <span>@ 2021 SHOPPINGCAT All rights reserved</span>
+                </div>
             </div>
         </div>
     </>
